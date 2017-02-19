5d2eb8d21e9bd0c7fe921fefde6608a26d961eae
tiagoslucas <tiago.slucas@gmail.com>
2017-02-19 03:20:16 +0000

    Finished (Inc. Extra-Cred)

--- server.py
+++ server.py
@@ -1,15 +1,55 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import sqlite3
+from os.path import isfile
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    if isfile('database.db') == False:
+        exec(open('initdb.py').read())
+    return render_template('newmovie.html')
+
+@app.route('/newmovie', methods = ['POST'])
+def newmovie():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    
+    title = request.form['title']
+    year = request.form['year']
+    
+    try:
+        cursor.execute('INSERT INTO movies (title,year) VALUES (?,?)', (title,year))
+        connection.commit()
+        message = 'Success!'
+    except:
+        connection.rollback()
+        message = 'Oops!'
+    finally:
+        connection.close()
+        return render_template('result.html', message = message)
 
 @app.route('/movies')
 def movies():
-    print('')
-    
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM movies')
+    cinematography = cursor.fetchall()
+    connection.close()
+    return jsonify(cinematography)
+
+@app.route('/search')
+def serach():
+    partial = request.args.get('title')
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM movies WHERE title LIKE "%{}%" COLLATE NOCASE'.format(partial))
+    foundit = cursor.fetchall()
+    connection.close()
+    return jsonify(foundit)
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
     
 app.run(debug = True)
